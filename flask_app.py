bc900367566b57c0d9ad1b89add82fdb00cca127
zawsze-razem <61657780+zawsze-razem@users.noreply.github.com>
2020-03-01 15:53:18 +0100

    mongo support and home page posts

--- flask_app.py
+++ flask_app.py
@@ -1,52 +1,28 @@
 from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/public"
+mongo = PyMongo(app)
 
-
-dummy_data = [
-
-    {
-        'rank' : 1,
-        'player' : 'suri',
-        'points' : 999
-    },
-    {
-        'rank' : 2,
-        'player' : 'Zawsze Razem',
-        'points' : 997
-    },
-    {
-        'rank' : 3,
-        'player' : 'Aracurya',
-        'points' : 777
-    },
-    {
-        'rank' : 4,
-        'player' : 'MUM',
-        'points' : 69
-    },
-    {
-        'rank' : 5,
-        'player' : 'H',
-        'points' : 4
-    },
-
-]
 
 
 # The main landing page
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    data = mongo.db.home.find()
+    return render_template('home.html', data=data)
 
 # Page displaying the ranking
 @app.route('/manhunt')
 def manhunt():
-    return render_template('ranking.html', data=dummy_data, title = 'Manhunt | Assassins\' Network', mode='Manhunt' )
+    data = mongo.db.players.find().sort("mhmmr",-1)
+    return render_template('ranking.html', data=data, title = 'Manhunt | Assassins\' Network', mode='Manhunt' )
 
 @app.route('/escort')
 def escort():
-    return render_template('ranking.html', data=dummy_data, title = 'Escort | Assassins\' Network', mode='Escort' )
+    data = mongo.db.players.find().sort("emmr",-1)
+    return render_template('ranking.html', data=data, title = 'Escort | Assassins\' Network', mode='Escort' )
 
 # Page showing the algorithm used
 @app.route('/elo')
