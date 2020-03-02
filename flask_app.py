efc2ac72a99fe3974db6cca864247f4acc3b8557
zawsze-razem <61657780+zawsze-razem@users.noreply.github.com>
2020-03-02 11:47:22 +0100

    player profiles etc

--- flask_app.py
+++ flask_app.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/public"
 mongo = PyMongo(app)
 
-
-
 # The main landing page
 @app.route('/')
 @app.route('/home')
@@ -33,6 +31,16 @@ def elo():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About Assassins\' Network')
+
+@app.route('/matches')
+def matches():
+    data = mongo.db.matches.find().sort("_id", -1)
+    return render_template('matches.html',data=data, title = 'Match History | Assassins\' Network')
+
+@app.route('/profile/<name>')
+def display_profile(name):
+    data = mongo.db.players.find_one({"ign": name})
+    return render_template('profile.html',data=data, title = 'Player\'s Profile | Assassins\' Network')
 
 if __name__ == '__main__':
     app.run(debug=True)
