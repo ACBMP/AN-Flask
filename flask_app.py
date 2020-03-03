f82b7cb270a0ebc57c9e51b04e3379ab4941b64f
zawsze-razem <61657780+zawsze-razem@users.noreply.github.com>
2020-03-03 22:52:07 +0100

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -62,5 +62,4 @@ def winrate(w,l):
 
 # WE CAN'T RUN A LIVE APP IN DEBUG MODE! 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
